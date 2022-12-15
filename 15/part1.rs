b10fb458ae23a5ed0bfd2ab2450aa2a50b056151
qoheniac <29815186+qoheniac@users.noreply.github.com>
2022-12-15 20:03:11 +0100

    move prefix stripping to a function

--- 15/part1.rs
+++ 15/part1.rs
@@ -1,7 +1,11 @@
 use std::{collections::HashSet, fs::read_to_string};
 
-const SENSOR_PREFIX: &str = "Sensor at";
+const SENSOR_PREFIX: &str = "Sensor at ";
 const BEACON_PREFIX: &str = "closest beacon is at ";
+
+fn strip_next<'a>(split: &'a mut std::str::Split<&str>, prefix: &str) -> &'a str {
+    split.next().unwrap().strip_prefix(prefix).unwrap()
+}
 
 fn main() {
     let contents = read_to_string("input").unwrap();
@@ -9,46 +13,12 @@ fn main() {
     let mut sensors = HashSet::new();
     for line in contents.lines() {
         let mut devices = line.split(": ");
-        let mut sensor_loc = devices
-            .next()
-            .unwrap()
-            .strip_prefix(SENSOR_PREFIX)
-            .unwrap()
-            .split(", ");
-        let sensor_x: usize = sensor_loc
-            .next()
-            .unwrap()
-            .strip_prefix("x=")
-            .unwrap()
-            .parse()
-            .unwrap();
-        let sensor_y: usize = sensor_loc
-            .next()
-            .unwrap()
-            .strip_prefix("y=")
-            .unwrap()
-            .parse()
-            .unwrap();
-        let mut beacon_loc = devices
-            .next()
-            .unwrap()
-            .strip_prefix(BEACON_PREFIX)
-            .unwrap()
-            .split(", ");
-        let beacon_x: usize = beacon_loc
-            .next()
-            .unwrap()
-            .strip_prefix("x=")
-            .unwrap()
-            .parse()
-            .unwrap();
-        let beacon_y: usize = beacon_loc
-            .next()
-            .unwrap()
-            .strip_prefix("y=")
-            .unwrap()
-            .parse()
-            .unwrap();
+        let mut sensor_loc = strip_next(&mut devices, SENSOR_PREFIX).split(", ");
+        let sensor_x: i32 = strip_next(&mut sensor_loc, "x=").parse().unwrap();
+        let sensor_y: i32 = strip_next(&mut sensor_loc, "y=").parse().unwrap();
+        let mut beacon_loc = strip_next(&mut devices, BEACON_PREFIX).split(", ");
+        let beacon_x: i32 = strip_next(&mut beacon_loc, "x=").parse().unwrap();
+        let beacon_y: i32 = strip_next(&mut beacon_loc, "y=").parse().unwrap();
         beacons.insert([beacon_x, beacon_y]);
         sensors.insert([sensor_x, sensor_y, beacon_x, beacon_y]);
     }
