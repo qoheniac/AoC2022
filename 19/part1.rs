54d736812228b1ccace5232c1c02eb51bc12f2e9
qoheniac <29815186+qoheniac@users.noreply.github.com>
2022-12-21 01:59:03 +0100

    restructure data

--- 19/part1.rs
+++ 19/part1.rs
@@ -1,40 +1,41 @@
 use std::{collections::HashMap, fs::read_to_string};
 
-enum MaterialCost {
-    Ore(u8),
-    Clay(u8),
-    Obsidian(u8),
+#[derive(Eq, Hash, PartialEq)]
+enum Material {
+    Ore,
+    Clay,
+    Obs,
 }
+use Material::*;
 
-struct Blueprint {
-    ore_robot_costs: Vec<MaterialCost>,
-    clay_robot_costs: Vec<MaterialCost>,
-    obsidian_robot_costs: Vec<MaterialCost>,
-    geode_robot_costs: Vec<MaterialCost>,
+#[derive(Eq, Hash, PartialEq)]
+enum Robot {
+    OreBot,
+    ClayBot,
+    ObsBot,
+    GeodeBot,
 }
+use Robot::*;
+
+type Resources = HashMap<Material, u8>;
+type Blueprint = HashMap<Robot, Resources>;
 
 fn main() {
     let contents = read_to_string("input").unwrap();
-    let mut blueprints = HashMap::new();
+    let mut blueprints: HashMap<u8, Blueprint> = HashMap::new();
     contents.lines().for_each(|line| {
-        let numbers: Vec<u8> = line
+        let vals: Vec<u8> = line
             .split(|c: char| !c.is_numeric())
             .filter_map(|s| s.parse().ok())
             .collect();
         blueprints.insert(
-            numbers[0],
-            Blueprint {
-                ore_robot_costs: vec![MaterialCost::Ore(numbers[1])],
-                clay_robot_costs: vec![MaterialCost::Ore(numbers[2])],
-                obsidian_robot_costs: vec![
-                    MaterialCost::Ore(numbers[3]),
-                    MaterialCost::Clay(numbers[4]),
-                ],
-                geode_robot_costs: vec![
-                    MaterialCost::Ore(numbers[5]),
-                    MaterialCost::Obsidian(numbers[6]),
-                ],
-            },
+            vals[0],
+            HashMap::from([
+                (OreBot, HashMap::from([(Ore, vals[1])])),
+                (ClayBot, HashMap::from([(Ore, vals[2])])),
+                (ObsBot, HashMap::from([(Ore, vals[3]), (Clay, vals[4])])),
+                (GeodeBot, HashMap::from([(Ore, vals[5]), (Obs, vals[6])])),
+            ]),
         );
     });
 }
