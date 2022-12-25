a98b6847d91b90d827f885534cb07f90a99c6050
qoheniac <29815186+qoheniac@users.noreply.github.com>
2022-12-25 19:25:59 +0100

    find solution by inverting operations

--- 21/part2.rs
+++ 21/part2.rs
@@ -1,39 +1,75 @@
-use std::{
-    collections::HashMap,
-    fs::read_to_string,
-    ops::{Add, Div, Mul, Sub},
-};
-
-type Operation = fn(u64, u64) -> u64;
+use std::{collections::HashMap, fs::read_to_string};
 
 #[derive(Clone, Copy)]
 enum Job<'a> {
-    Number(u64),
-    Computation(Operation, &'a str, &'a str),
+    Number(Option<u64>),
+    Computation(&'a str, &'a str, &'a str),
 }
 use Job::*;
 
-fn parse_operation(symbol: &str) -> Operation {
-    match symbol {
-        "+" => u64::add,
-        "-" => u64::sub,
-        "*" => u64::mul,
-        "/" => u64::div,
+fn apply_op(op: &str, num1: u64, num2: u64) -> u64 {
+    match op {
+        "+" => num1 + num2,
+        "-" => num1 - num2,
+        "*" => num1 * num2,
+        "/" => num1 / num2,
         _ => panic!(),
     }
 }
 
-fn yell<'a>(monkey: &'a str, jobs: &mut HashMap<&'a str, Job<'a>>) -> u64 {
+fn yell<'a>(monkey: &'a str, jobs: &mut HashMap<&'a str, Job<'a>>) -> Option<u64> {
     let job = *jobs.get(monkey).unwrap();
     match job {
-        Number(num) => num,
+        Number(opt) => opt,
         Computation(op, monkey1, monkey2) => {
-            let num1 = yell(monkey1, jobs);
-            let num2 = yell(monkey2, jobs);
-            let num = op(num1, num2);
-            jobs.insert(monkey, Number(num));
-            num
+            let mut opt = None;
+            if let Some(num1) = yell(monkey1, jobs) {
+                if let Some(num2) = yell(monkey2, jobs) {
+                    opt = Some(apply_op(op, num1, num2));
+                    jobs.insert(monkey, Number(opt));
+                }
+            }
+            opt
         }
+    }
+}
+
+fn find_humn_number<'a>(monkey: &'a str, ans: u64, jobs: &mut HashMap<&'a str, Job<'a>>) -> u64 {
+    let job = *jobs.get(monkey).unwrap();
+    if let Computation(op, monkey1, monkey2) = job {
+        let mut next_monkey = monkey1;
+        let other_ans = if let Some(num) = yell(monkey1, jobs) {
+            next_monkey = monkey2;
+            num
+        } else {
+            yell(monkey2, jobs).unwrap()
+        };
+        let next_ans = match op {
+            "+" => ans - other_ans, // ans = next + other (or v.v.)
+            "-" => {
+                if next_monkey == monkey1 {
+                    ans + other_ans // ans = next - other
+                } else {
+                    other_ans - ans // ans = other - next
+                }
+            }
+            "*" => ans / other_ans, // ans = next * other (or v.v.)
+            "/" => {
+                if next_monkey == monkey1 {
+                    ans * other_ans // ans = next / other
+                } else {
+                    other_ans / ans // ans = other / next
+                }
+            }
+            _ => panic!(),
+        };
+        if next_monkey == "humn" {
+            next_ans
+        } else {
+            find_humn_number(next_monkey, next_ans, jobs)
+        }
+    } else {
+        panic!();
     }
 }
 
@@ -46,26 +82,13 @@ fn main() {
         jobs.insert(
             name,
             if parts.len() == 2 {
-                Number((*parts[1]).parse::<u64>().unwrap())
+                Number(Some((*parts[1]).parse::<u64>().unwrap()))
             } else {
-                Computation(parse_operation(parts[2]), parts[1], parts[3])
+                let op = if name == "root" { "-" } else { parts[2] };
+                Computation(op, parts[1], parts[3])
             },
         );
     }
-    let root_job = jobs.remove("root").unwrap();
-    if let Computation(_, monkey1, monkey2) = root_job {
-        let mut jobs_copy = jobs.clone();
-        for num in 0..100000 {
-            jobs_copy.insert("humn", Number(num));
-            let num1 = yell(monkey1, &mut jobs_copy);
-            let num2 = yell(monkey2, &mut jobs_copy);
-            if num1 == num2 {
-                println!("{}", num);
-                break;
-            }
-            for (monkey, job) in &jobs {
-                *jobs_copy.get_mut(monkey).unwrap() = job.clone();
-            }
-        }
-    }
+    jobs.insert("humn", Number(None));
+    println!("{}", find_humn_number("root", 0, &mut jobs))
 }
